b1149135a4abf2d39454beef46d9398840083e59
Niranjan <120588092+sonu-2004@users.noreply.github.com>
2025-06-16 02:36:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ burger.addEventListener('click', () => {
   navLinks.classList.toggle('nav-active');
   burger.classList.toggle('toggle');
 });
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('appear');
+    observer.unobserve(entry.target);
+  });
+}, { threshold: 0.1 });
+
+faders.forEach(fader => appearOnScroll.observe(fader));
